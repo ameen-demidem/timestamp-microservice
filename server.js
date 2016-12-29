07ceb09ab08464edd9c1745be6b360b8e4d06839
Ameen Demidem <ameen.demidem@gmail.com>
2016-12-28 22:13:09 -0500

    Respond with unix time and a natural date when sent a proper request

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const express = require('express');
 
 const app = express();
+
 app.get('/', function (req, res) {
-  res.end('Hello World!');
+  res.end(
+    '<h1>Example usage</h1>' +
+      'http://localhost:3000/Jan 1 2016<br>' +
+      'http://localhost:3000/1450137600'
+  );
+});
+
+const Months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+];
+app.get('/:date', function (req, res) {
+  const date = isNaN(req.params.date) ?
+    new Date(req.params.date) :
+    new Date(Number(req.params.date));
+  let natural = null, unix = null;
+  let result = {unix, natural};
+
+  if (!isNaN(date.getTime())) {
+    const day = date.getDate();
+    const month = Months[date.getMonth()];
+    const year = date.getFullYear();
+
+    result = {
+      unix: date.getTime(),
+      natural: month + ' ' + day + ', ' + year
+    };
+  }
+
+  res.type('application/json');
+  res.end(JSON.stringify(result));
 });
 
 app.listen(3000, function () {
