6cdd9cf4a9dd22a54bc4e330cd52ebf6ba783975
Ameen Demidem <ameen.demidem@gmail.com>
2017-01-06 11:10:53 -0500

    Allow CORS

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 app.set('port', (process.env.PORT || 3000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
   res.end(
     '<h1>Example usage</h1>' +
